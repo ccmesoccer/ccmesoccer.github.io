1e090b12d6c91aca87a2adb56e947ac68c185eca
yufei <yfwu0202@gmail.com>
2021-08-16 12:42:40 +0800

    完成了数据统计页面

--- js/resultLight.js
+++ js/resultLight.js
@@ -3,6 +3,7 @@
 // Last Edited on 16/08/2021
 
 function resultLight(result) {
+	if(result=="-") return result;  // 未来的比赛
 	if(result.indexOf("(")!=-1) {
 		result = eval(result.slice(result.indexOf("(")+1, -1))
 	}  // 检查是否存在点球决胜
